131fb557838a170390adbecf977c18642d1c8773
asianParadissseee <asian.paradissseee@gmail.com>
2025-03-24 09:59:37 +0500

    added configs for husky,prettier,eslint

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <div class="text-red-500 text-3xl">
-    hello world {{a}}
-  </div>
-
+  <div class="text-red-500 text-3xl">hello world</div>
 </template>
 
 <style scoped></style>
